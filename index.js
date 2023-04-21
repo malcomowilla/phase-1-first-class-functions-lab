367133a1f004f2b6122c5eaa41b6fd61761308c4
malcomowilla <owillamalcom@gmail.com>
2023-04-21 19:02:36 +0300

    solved

--- index.js
+++ index.js
@@ -1,26 +1,87 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+
+// const returnFirstTwoDrivers = function (drivers){
+//     return drivers.slice (0, 2);
+
+// }
+// const returnLastTWoDrivers = function (drivers){
+//     return drivers.slice(2);
+
+// }
+
+// const selectDrivers = [returnFirstTWoDrivers , returnLastTwoDrivers];
+
+// const createFareMultiplier = function(fareMultiplier){
+//     return function (fare){
+//         return fare + fareMultiplier
+
+//     }
+// }
+
+// const fareDoubler = createFareMultiplier (2)
+// const fareTripler = createFareMultipler (3)
+
+// const selectDifferentDrivers = function (drivers, returnDrivers){
+//     return returnDrivers (drivers);
+// }
+
+
+
+
+
+
+// const returnFirstTWoDrivers = function (drivers){
+//      return drivers.slice (0,  2)
+     
+// }
+
 
 const returnFirstTwoDrivers = function (drivers){
-    return drivers.slice (0, 2);
-
-}
-const returnLastTWoDrivers = function (drivers){
-    return drivers.slice(2);
-
+     return drivers.slice (0, 2);
 }
 
-const selectDrivers = [returnFirstTWoDrivers , returnLastTwoDrivers];
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(0 -2)
+}
 
-const createFareMultiplier = function(fareMultiplier){
-    return function (fare){
-        return fare + fareMultiplier
 
+let selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+
+
+function createFareMultiplier(integer) {
+    return function name() {
+        let number = 5
+        return integer * number
     }
 }
 
-const fareDoubler = createFareMultiplier (2)
-const fareTripler = createFareMultipler (3)
 
-const selectDifferentDrivers = function (drivers, returnDrivers){
-    return returnDrivers (drivers);
+
+
+
+
+
+const fareDoubler = function(name){
+
+    return name * 2
 }
+
+
+
+const fareTripler = function (name) {
+    return name * 3
+}
+
+function selectDifferentDrivers(drivers, returnFirstTWoDrivers) {
+     return returnFirstTWoDrivers(drivers)
+     
+}
+
+
+
+
+
